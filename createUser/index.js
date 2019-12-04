326e268ad1340627d0db042a5a273ec6d5bc0c8e
Hambloko <hambloko@gmail.com>
2019-12-04 16:21:50 -0600

    - Added UUID package to create ID's
    - Placeholder event modification

--- createUser/index.js
+++ createUser/index.js
@@ -1,9 +1,17 @@
 const AWS = require('aws-sdk');
+const uuidv4 = require('uuid/v4');
 
 exports.handler = async(event) => {
+    const body = {
+        email: "someguy@gmail.com",
+        password: "123123"
+    }
+    const newEvent = {...event, body: JSON.stringify(body)};
+
+    
     const response = {
         code: 200,
-        body: JSON.stringify(event)
+        body: JSON.stringify(newEvent)
     }
     return response;
 };
