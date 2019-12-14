fbf09cdcae21a40153bfd13132c0bbb24bf7dbb8
Joseph Cooper <hambloko@gmail.com>
2019-12-14 00:26:13 -0600

    - logIn function completed

--- logIn/index.js
+++ logIn/index.js
@@ -7,59 +7,99 @@ const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10" });
 const secret = process.env.secret;
 
 const validate = input => {
-    return new Promise((resolve, reject) => {
-        const user = JSON.parse(input);
-        if (!user.email || !user.password) {
-            reject({ code: 400, msg: "Error: Please submit an email and a password"});
-            return;
-        }
-        resolve(user);
-    })
-}
+  return new Promise((resolve, reject) => {
+    if (!input.email || !input.password) {
+      reject({
+        code: 400,
+        msg: "Error: Please submit an email and a password"
+      });
+      return;
+    }
+    resolve(input);
+  });
+};
 
 const getUser = input => {
-    return new Promise((resolve, reject) => {
-        const params = {
-            TableName: "Users",
-            Key: {
-                "email": input.email
-            }
-        }
-        ddb.get(params, (err, data) => {
-            if (err) {
-                console.log(`DDB ERROR: ${err}`);
-                reject({ code: 500, msg: "Internal server error"});
-                return;
-            }
-            if (!data.Item) {
-                reject({ code: 400, msg: "User does not exist"});
-                return;
-            }
-            resolve(data.Item);
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: "Users",
+      Key: {
+        email: input.email
+      }
+    };
+    ddb.get(params, (err, data) => {
+      if (err) {
+        console.log(`DDB ERROR: ${err}`);
+        reject({ code: 500, msg: "Internal server error" });
+        return;
+      }
+      if (!data.Item) {
+        reject({ code: 400, msg: "User does not exist" });
+        return;
+      }
+      resolve(data.Item);
+    });
+  });
+};
+
+const validatePassword = (hash, password) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, hash, (err, res) => {
+      if (err) {
+        console.log(`BCRYPT ERROR: ${err}`);
+        reject({ code: 500, msg: "Internal server error" });
+        return;
+      }
+      if (res) {
+        resolve();
+        return;
+      } else {
+        reject({ code: 400, msg: "Passwords do not match" });
+        return;
+      }
+    });
+  });
+};
+
+const createToken = user => {
+  return new Promise((resolve, reject) => {
+    const payload = {
+      email: user.email,
+      deck: user.deck,
+      archived: user.archived || [],
+      id: user.id
+    };
+
+    jwt.sign(payload, secret, { expiresIn: "7d" }, (err, token) => {
+      if (err) {
+        console.log(`JWT ERR: ${err}`);
+        reject({ code: 500, msg: "Internal server error" });
+        return;
+      }
+      resolve(token);
+    });
+  });
+};
 
 exports.handler = async event => {
-    const body = JSON.stringify({
-        email: "auser1@gmail.com",
-        password: "123123"
-    })
+  const input = JSON.parse(event.body);
 
-    try {
-        const validated = await validate(body);
-        const user = await getUser(validated);
-        console.log(user);
-        return {
-            statusCode: 200,
-            body: JSON.stringify("Got 'em"),
-            isBase64Encoded: false
-        }
-    } catch (err) {
-        return {
-            statusCode: err.code,
-            body: JSON.stringify(err.msg),
-            isBase64Encoded: false
-        }
-    }
-}
+  try {
+    const validated = await validate(input);
+    const user = await getUser(validated);
+    await validatePassword(user.password, input.password);
+    const token = await createToken(user);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(token),
+      isBase64Encoded: false
+    };
+  } catch (err) {
+    return {
+      statusCode: err.code,
+      body: JSON.stringify(err.msg),
+      isBase64Encoded: false
+    };
+  }
+};
